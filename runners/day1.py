d6ff70d14531a36a6148f56167729e56808a493c
jellsprout <jelmern@hotmail.com>
2020-12-02 10:44:48 +0100

    !fixup Day1

--- runners/day1.py
+++ runners/day1.py
@@ -6,9 +6,9 @@ def execute_part(part):
     input_text = tools.common.get_input(1, 1)
     if part == 1:
         input_numbers = tools.common.convert_text_to_list_of_integers(input_text)
-        i, j = scripts.basic.find_sum(input_numbers, 2020)
+        i, j = scripts.basic.find_sum_components(input_numbers, 2020)
         return i*j
     if part == 2:
         input_numbers = tools.common.convert_text_to_list_of_integers(input_text)
-        output_numbers = scripts.basic.find_sum(input_numbers, 2020, 3)
+        output_numbers = scripts.basic.find_sum_components(input_numbers, 2020, 3)
         return scripts.basic.get_product(output_numbers)
