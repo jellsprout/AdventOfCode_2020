a1c70f2150b5e5865b4f67c3f361d11a41e52792
jellsprout <jelmern@hotmail.com>
2020-12-05 11:26:02 +0100

    !fixup Day 5

--- scripts/boarding_pass_scanner.py
+++ scripts/boarding_pass_scanner.py
@@ -3,8 +3,7 @@ class BoardingPassScanner:
         self.boarding_pass_string = boarding_pass_string.lower()
         self.boarding_pass_bin = self.boarding_pass_string.replace('f', '0').replace('b', '1').replace('l', '0')\
             .replace('r', '1')
-        self.row = int(self.boarding_pass_bin[:7], 2)
-        self.column = int(self.boarding_pass_bin[-4:], 2)
+        self.seat_id = int(self.boarding_pass_bin, 2)
 
     def get_seat_id(self):
-        return self.row * 8 + self.column
+        return self.seat_id
